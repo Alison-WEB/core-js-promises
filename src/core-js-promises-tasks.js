e9addd52b97bbb230d3f1911a02f1b92facaddf1
Alison WEB <AlisonWEB@yandex.ru>
2024-04-15 23:28:09 +0300

    feat: queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -163,8 +163,18 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = '';
+  function resolvePromise(index) {
+    if (index >= promises.length) {
+      return Promise.resolve(result);
+    }
+    return promises[index].then((content) => {
+      result += content;
+      return resolvePromise(index + 1);
+    });
+  }
+  return resolvePromise(0);
 }
 
 module.exports = {
